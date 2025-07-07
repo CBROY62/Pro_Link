5c56b594a9c52d788e2ffaa34d9dde880a369e69
CBROY62 <chandrabhushankumar27080@gmail.com>
2025-07-07 23:12:55 +0530

    add new paragraph

--- src/component/Fillter.jsx
+++ src/component/Fillter.jsx
@@ -17,7 +17,7 @@ function ProjectCard({ title, description, image, buttonLabel, link,demoCodeLabe
   )}
 
   {/* New Demo Code button */}
-  
+
   {(demoCodeLabel && demoCodeLink) && (
     <a href={demoCodeLink} target="_blank" rel="noopener noreferrer">
       <button className="project-button">{demoCodeLabel}</button>
@@ -64,6 +64,7 @@ export default function ProjectFilter() {
       </marquee>
 
       {/* Filter Buttons */}
+      
       <div className="filter-buttons">
         {["all", "minor", "mid", "major"].map((type) => (
           <button
